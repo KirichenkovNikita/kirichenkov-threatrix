f38cb2853a53938a76635232db4917e71310679d
Kirichenkov Nikita <47207005+KirichenkovNikita@users.noreply.github.com>
2025-07-03 21:17:00 +0400

    update

--- mixed1.c
+++ mixed1.c
@@ -1,3 +1,23 @@
+/*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Repositories:
+*     - arch/arm/core/
+*     - src/
+*    
+*    Copyrights:
+*    
+*    
+*    Licenses:
+*     - Eclipse Public License 2.0
+*       SPDXId: EPL-2.0
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
 
 
 // https://github.com/eclipse/mosquitto/blob/v2.0.1/src/control.c
